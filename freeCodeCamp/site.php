78f114701c66db5b47c8bfbed8054d1ec7c67a6b
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-10 13:43:48 +0200

    checkboxes

--- freeCodeCamp/site.php
+++ freeCodeCamp/site.php
@@ -6,9 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="site.php" method="POST">
+       Apples: <input type="checkbox" name="fruits[]" value="apples"> <br>
+       Oranges: <input type="checkbox" name="fruits[]" value="oranges"> <br>
+       Pears: <input type="checkbox" name="fruits[]" value="pears"> <br>
+       <input type="submit">
+    </form>
     <?php 
-       $friends = ['Mike', 'Owen', 'Karen', 'Harry'];
-       print_r($friends);
+       $fruits = $_POST['fruits'];
+       print_r($fruits);
     ?>
 </body>
 </html>
