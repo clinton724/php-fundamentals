4b32ac77d2727984f7a58fdeb4c75e946953a0c9
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-10 12:56:05 +0200

    Mad Libs game

--- freeCodeCamp/site.php
+++ freeCodeCamp/site.php
@@ -7,13 +7,18 @@
 </head>
 <body>
     <form action="site.php" method="GET">
-        <input type="number" name="num1">
-        <br>
-        <input type="number" name="num2">
+        Color: <input type="text" name="color"> <br>
+        Plural Noun: <input type="text" name="pluralNoun"> <br>
+        Celebrity: <input type="text" name="celebrity">
        <input type="submit">
     </form>
     <?php 
-       echo "Answer: ".$_GET['num1']+$_GET['num2'];
+       $color = $_GET['color'];
+       $pluralNoun = $_GET['pluralNoun'];
+       $celebrity = $_GET['celebrity'];
+       echo "Roses are $color <br>";
+       echo "$pluralNoun are blue <br>";
+       echo "I love $celebrity <br>";
     ?>
 </body>
 </html>
