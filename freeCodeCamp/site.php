895cd04503a9e474135f788f21991f531150ad93
Clinton Letsoela <clintonletsoela724c@gmail.com>
2023-08-10 13:13:00 +0200

    using post instead of get

--- freeCodeCamp/site.php
+++ freeCodeCamp/site.php
@@ -6,19 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="site.php" method="GET">
-        Color: <input type="text" name="color"> <br>
-        Plural Noun: <input type="text" name="pluralNoun"> <br>
-        Celebrity: <input type="text" name="celebrity">
+    <form action="site.php" method="POST">
+        Password: <input type="password" name="password"> <br>
        <input type="submit">
     </form>
     <?php 
-       $color = $_GET['color'];
-       $pluralNoun = $_GET['pluralNoun'];
-       $celebrity = $_GET['celebrity'];
-       echo "Roses are $color <br>";
-       echo "$pluralNoun are blue <br>";
-       echo "I love $celebrity <br>";
+       echo $_POST['password'];
     ?>
 </body>
 </html>
